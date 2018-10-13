c18614129fcea59f7d1050f42e2f4bbac7fb16e2
marco <goethe@marco-fuchs.de>
2018-10-13 13:59:53 +0200

    changed expression evaluator

--- STFU.Lib.Youtube.Automation/Programming/ExpressionEvaluator.cs
+++ STFU.Lib.Youtube.Automation/Programming/ExpressionEvaluator.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
 using MoonSharp.Interpreter;
+using STFU.Lib.Youtube.Automation.Interfaces.Model;
 using mss = MoonSharp.Interpreter;
 
 namespace STFU.Lib.Youtube.Automation.Programming
 {
 	public class ExpressionEvaluator
 	{
+		private IReadOnlyDictionary<string, Func<string, string, string>> GlobalVariables =>
+			new ReadOnlyDictionary<string, Func<string, string, string>>(new Dictionary<string, Func<string, string, string>>
+		{
+			{ "file", (path, template) => path },
+			{ "filename", (path, template) => Path.GetFileNameWithoutExtension(path) },
+			{ "fileext", (path, template) => new FileInfo(path).Extension },
+			{ "filenameext", (path, template) => new FileInfo(path).Name },
+			{ "folder", (path, template) => new FileInfo(path).DirectoryName },
+			{ "foldername", (path, template) => new FileInfo(path).Directory.Name },
+			{ "template", (path, template) => template }
+		});
+
 		private string FilePath { get; set; }
 		private string TemplateName { get; set; }
 		private string CSharpPreparationScript { get; set; }
@@ -21,10 +34,13 @@ namespace STFU.Lib.Youtube.Automation.Programming
 
 		private ScriptState<object> CsScript { get; set; }
 
-		public ExpressionEvaluator(string filepath, string templatename, string csharpPreparationScript, string csharpCleanupScript)
+		private IList<IPlannedVideo> PlannedVideos { get; set; } = new List<IPlannedVideo>();
+
+		public ExpressionEvaluator(string filepath, string templatename, IList<IPlannedVideo> plannedVideos, string csharpPreparationScript, string csharpCleanupScript)
 		{
 			FilePath = filepath;
 			TemplateName = templatename;
+			PlannedVideos = plannedVideos;
 
 			CSharpPreparationScript = csharpPreparationScript;
 			CSharpCleanupScript = csharpCleanupScript;
@@ -34,7 +50,7 @@ namespace STFU.Lib.Youtube.Automation.Programming
 
 		private async Task CreateExpressionEvaluator()
 		{
-			foreach (var var in Variable.GlobalVariables)
+			foreach (var var in GlobalVariables)
 			{
 				string func = $"const string {var.Key} = @\"{var.Value(FilePath, TemplateName)}\";";
 
@@ -55,7 +71,7 @@ namespace STFU.Lib.Youtube.Automation.Programming
 			catch (CompilationErrorException ex)
 			{
 				CsScript = await CSharpScript.RunAsync("using System;");
-				
+
 				if (!Directory.Exists("errors"))
 				{
 					Directory.CreateDirectory("errors");
@@ -124,7 +140,7 @@ namespace STFU.Lib.Youtube.Automation.Programming
 						int closingPos = FindClosingPosition(text, currentPos);
 						if (closingPos > currentPos)
 						{
-							var replacement = EvaluateExpression(text.Substring(currentPos + 1, closingPos - currentPos - 1));
+							var replacement = EvaluateField(text.Substring(currentPos + 1, closingPos - currentPos - 1));
 							text = $"{text.Substring(0, currentPos)}{replacement}{text.Substring(closingPos + 1)}";
 						}
 					}
@@ -256,252 +272,19 @@ namespace STFU.Lib.Youtube.Automation.Programming
 			return text.IndexOf(">>>", currentPos);
 		}
 
-		private string EvaluateExpression(string expression)
+		private string EvaluateField(string expression)
 		{
-			expression = expression.Trim();
-
-			// Innere Expressions auswerten
-			for (int currentPos = 0; currentPos < expression.Length; currentPos++)
-			{
-				if (expression[currentPos] == '<')
-				{
-					int closingPos = FindClosingPosition(expression, currentPos);
-					if (closingPos > currentPos)
-					{
-						var replacement = EvaluateExpression(expression.Substring(currentPos + 1, closingPos - currentPos - 1));
-						expression = $"{expression.Substring(0, currentPos)}{replacement}{expression.Substring(closingPos + 1)}";
-					}
-				}
-			}
-
 			string result = string.Empty;
-			StringBuilder currentExpression = new StringBuilder();
-			for (int currentPos = 0; currentPos < expression.Length; currentPos++)
+
+			expression = expression.Trim();
+			var fileName = Path.GetFileNameWithoutExtension(FilePath).ToLower();
+			var fileNameExt = Path.GetFileName(FilePath).ToLower();
+
+			var plannedVideo = PlannedVideos.FirstOrDefault(v => v.Name.ToLower() == fileName || v.Name.ToLower() == fileNameExt);
+
+			if (plannedVideo != null && plannedVideo.Fields.ContainsKey(expression))
 			{
-				char currentCharacter = expression[currentPos];
-
-				if (currentCharacter == '(')
-				{
-					// Funktion
-					List<string> arguments = new List<string>();
-					string currentArgument = string.Empty;
-					for (int functionEndPos = currentPos + 1; functionEndPos < expression.Length; functionEndPos++)
-					{
-						char currentChar = expression[functionEndPos];
-
-						if (currentChar == ')')
-						{
-							arguments.Add(currentArgument);
-
-							currentPos = functionEndPos;
-							result = EvaluateFunction(result, currentExpression.ToString(), arguments);
-							currentExpression = new StringBuilder();
-							break;
-						}
-						else if (currentChar == ',')
-						{
-							currentArgument += currentChar;
-							arguments.Add(currentArgument);
-							currentArgument = string.Empty;
-						}
-						else
-						{
-							currentArgument += currentChar;
-						}
-					}
-				}
-				else if (currentCharacter == '.')
-				{
-					if (currentExpression.Length > 0)
-					{
-						// Identifier => auswerten
-						result = EvaluateIdentifier(currentExpression.ToString());
-						currentExpression = new StringBuilder();
-					}
-				}
-				else
-				{
-					currentExpression.Append(currentCharacter);
-				}
-			}
-
-			if (currentExpression.Length > 0)
-			{
-				// Identifier => auswerten
-				result = EvaluateIdentifier(currentExpression.ToString());
-			}
-
-			return result;
-		}
-
-		private string EvaluateIdentifier(string identifier)
-		{
-			string result = identifier;
-
-			if (Variable.GlobalVariables.ContainsKey(identifier.ToLower()))
-			{
-				result = Variable.GlobalVariables[identifier.ToLower()](FilePath, TemplateName);
-			}
-
-			return result;
-		}
-
-		private string EvaluateFunction(string result, string function, List<string> arguments)
-		{
-			switch (function.ToLower())
-			{
-				case "readfile":
-					result = ReadFile(result, arguments);
-					break;
-				case "findnumber":
-					result = FindNumber(result, arguments);
-					break;
-				case "substring":
-					result = FindSubstring(result, arguments);
-					break;
-				case "removeleadingzeros":
-					result = RemoveLeadingZeros(result);
-					break;
-				case "getpassage":
-					result = GetPassage(result, arguments);
-					break;
-				default:
-					break;
-			}
-
-			return result;
-		}
-
-		private static string GetPassage(string result, List<string> arguments)
-		{
-			if (arguments.Count > 0)
-			{
-				string passageName = arguments.First();
-
-				var splits = result.Split(new[] { $"<{passageName}>" }, StringSplitOptions.None);
-				if (splits.Length > 1)
-				{
-					result = splits[1];
-				}
-			}
-
-			return result;
-		}
-
-		private static string RemoveLeadingZeros(string result)
-		{
-			int removecount = 0;
-			for (int i = 0; i < result.Length; i++)
-			{
-				if (result[i] == '0')
-				{
-					removecount++;
-				}
-				else
-				{
-					break;
-				}
-			}
-			result = result.Substring(removecount);
-
-			if (result.Length == 0 && removecount > 0)
-			{
-				result = "0";
-			}
-
-			return result;
-		}
-
-		private static string FindSubstring(string result, List<string> arguments)
-		{
-			int start = 0;
-			int end = 0;
-			if (arguments.Count == 1 && int.TryParse(arguments.First(), out end) && end >= 0)
-			{
-				// kein <= weil result.substring(0, end) == result, wenn end == result.length
-				// => unnötige Operation
-				if (end < result.Length)
-				{
-					result = result.Substring(0, end);
-				}
-			}
-			else if (arguments.Count > 1
-				&& int.TryParse(arguments[0], out start)
-				&& int.TryParse(arguments[1], out end)
-				&& start >= 0
-				&& end >= start)
-			{
-				if (start <= result.Length - 1)
-				{
-					if (start + end < result.Length)
-					{
-						result = result.Substring(start, end);
-					}
-					else
-					{
-						result = result.Substring(start, result.Length - start);
-					}
-				}
-				else
-				{
-					result = string.Empty;
-				}
-			}
-
-			return result;
-		}
-
-		private static string FindNumber(string result, List<string> arguments)
-		{
-			int numberToFind = 0;
-			if (arguments.Count > 0)
-			{
-				int.TryParse(arguments.First(), out numberToFind);
-				numberToFind--;
-			}
-
-			var decimalSeparator = Convert.ToChar(Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator);
-
-			var numbers = new List<string>();
-			bool decimalSeparatorAppeared = false;
-			StringBuilder currentNumber = new StringBuilder();
-			foreach (var letter in result)
-			{
-				if (char.IsDigit(letter))
-				{
-					currentNumber.Append(letter);
-				}
-				else if (!decimalSeparatorAppeared && letter == decimalSeparator)
-				{
-					decimalSeparatorAppeared = true;
-					currentNumber.Append(letter);
-				}
-				else if (currentNumber.Length > 0)
-				{
-					numbers.Add(currentNumber.ToString());
-					decimalSeparatorAppeared = false;
-					currentNumber = new StringBuilder();
-				}
-			}
-
-			result = string.Empty;
-			if (numbers.Count >= numberToFind + 1)
-			{
-				result = numbers[numberToFind];
-			}
-
-			return result;
-		}
-
-		private static string ReadFile(string result, List<string> arguments)
-		{
-			if (arguments.Count > 0 && File.Exists(arguments.First()))
-			{
-				using (StreamReader reader = new StreamReader(arguments.First()))
-				{
-					// Keine Begrenzung auf 5k Zeichen, da er ja möglicherweise eine Passage lesen will.
-					result = reader.ReadToEnd();
-				}
+				result = plannedVideo.Fields[expression];
 			}
 
 			return result;
