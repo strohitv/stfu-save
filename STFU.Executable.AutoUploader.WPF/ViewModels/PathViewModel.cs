aa1abac613284b4d53f3d5bf43498e62bff52f0d
MaxPlay <maxplayyt@gmail.com>
2018-12-12 01:55:31 +0100

    Added icons for buttons (looks and works better compared to the text-approach) Fixed default column sizes for gridview on MainWindow Added ViewModel for PathWindow and designed PathWindow

---
+++ STFU.Executable.AutoUploader.WPF/ViewModels/PathViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STFU.Executable.AutoUploader.WPF.ViewModels
+{
+    public class PathViewModel : ViewModelBase
+    {
+        public void DoAction(PathWindowAction action)
+        {
+
+        }
+    }
+
+    public enum PathWindowAction
+    {
+        Add,
+        MoveUp,
+        MoveDown,
+        Delete,
+        Clear
+    }
+}
--- STFU.Executable.AutoUploader.WPF/Windows/PathWindow.xaml.cs
+++ STFU.Executable.AutoUploader.WPF/Windows/PathWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using STFU.Executable.AutoUploader.WPF.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace STFU.Executable.AutoUploader.WPF.Windows
 {
@@ -12,8 +14,37 @@ namespace STFU.Executable.AutoUploader.WPF.Windows
         public PathWindow()
         {
             InitializeComponent();
+            ViewModel = DataContext as PathViewModel;
         }
 
+        public PathViewModel ViewModel { get; private set; }
+
         #endregion Public Constructors
+
+        private void ActionButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            ViewModel.DoAction((PathWindowAction)button.Tag);
+        }
+
+        private void ChosePath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MarkAllVideos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
