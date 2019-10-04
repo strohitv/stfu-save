ddcfe9004393d91e88ef57d0c976f65f910a6bc7
strohitv <the.real.strohi@gmail.com>
2019-10-04 12:19:30 +0200

    prepare next version

--- STFU.Executable.StandardUploader/Properties/AssemblyInfo.cs
+++ STFU.Executable.StandardUploader/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("STFU.Executable.StandardUploader")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.8")]
-[assembly: AssemblyFileVersion("0.3.8")]
+[assembly: AssemblyVersion("0.3.9")]
+[assembly: AssemblyFileVersion("0.3.9")]
