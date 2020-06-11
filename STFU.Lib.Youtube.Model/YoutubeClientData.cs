7028770cdd5af5491e3958a792e29d800b1e1159
marco <goethe@marco-fuchs.de>
2020-06-11 12:01:10 +0200

    fixed untracked files

--- STFU.Lib.Youtube.Model/YoutubeClientData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace STFU.Lib.Youtube.Model
-{
-	public static class YoutubeClientData
-	{
-		public static YoutubeClient Client => new YoutubeClient("your api key",
-				   "your api secret",
-				   "your api name", false);
-	}
-}
