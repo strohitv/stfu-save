4e32767471e33e2ffd0617e6a1fb825868462ab1
strohitv <45733269+strohitv@users.noreply.github.com>
2020-06-19 01:01:31 +0200

    Update YoutubeClientData.cs

--- STFU.Lib.Youtube.Model/YoutubeClientData.cs
+++ STFU.Lib.Youtube.Model/YoutubeClientData.cs
@@ -2,9 +2,11 @@ namespace STFU.Lib.Youtube.Model
 {
 	public static class YoutubeClientData
 	{
-		public static YoutubeClient Client => new YoutubeClient("youtube api key here",
-				   "youtube api secret here",
+		public static YoutubeClient Client => new YoutubeClient("youtube oauth 2 key here",
+				   "youtube oauth 2 secret here",
 				   "youtube api project name here", false);
+		
+		public static string YoutubeApiKey => "youtube api key here";
 
 		public static string TwitterOauthToken => "twitter api key here";
 
